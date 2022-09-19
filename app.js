fcf3aa51135625445b7256e19e7f8e8305d50290
ivan2798 <inge1998@hotmail.com>
2022-09-19 12:29:11 -0500

    true y false

---
+++ app.js
@@ -0,0 +1,11 @@
+let verdadero = true;
+let falso = false;
+let v = Boolean(false);
+
+console.log(verdadero,falso,v);
+console.log(typeof verdadero,typeof falso);
+
+console.log(Boolean(0));
+console.log(Boolean(-5));
+console.log(Boolean(""));
+console.log(Boolean(" "));
